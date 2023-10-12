dfd0dfb8344df492985e6239f2ee5abbd0322f1c
sanre <sanre@X>
2023-10-11 19:33:20 -0500

    Se agregaron atributos

--- src/Stock.java
+++ src/Stock.java
@@ -1,5 +1,25 @@
 
 public class Stock {
+    private String nombreIngrediente;
+    private int cantidadIngrediente;
+
+    public String getNombreIngrediente() {
+        return nombreIngrediente;
+    }
+
+    public void setNombreIngrediente(String nombreIngrediente) {
+        this.nombreIngrediente = nombreIngrediente;
+    }
+
+    public int getCantidadIngrediente() {
+        return cantidadIngrediente;
+    }
+
+    public void setCantidadIngrediente(int cantidadIngrediente) {
+        this.cantidadIngrediente = cantidadIngrediente;
+    }
+    
+    
     
     
     public void contarIngredientes(){
